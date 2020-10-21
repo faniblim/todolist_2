c2775c86fe855c81f8d57f22cbe51789b8ec1917
faniblim <faniblim@mail.ru>
2020-10-21 21:24:04 +0300

    todolist 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TaskType, Todolist} from "./Todolist";
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
-    let [tasks, setTasks] = useState([
+    let [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
@@ -14,23 +14,23 @@ function App() {
         {id: 6, title: "GrahhQL", isDone: false},
 
     ])
+    let [filter, setFilter] = useState<FilterValuesType>("all");
 
     function removeTask(id: number) {
-        let filteredTasks = tasks.filter(t => t.id != id);
+        let filteredTasks = tasks.filter(t => t.id !== id);
         setTasks(filteredTasks);
     }
 
-    let [filter, setFilter] = useState<FilterValuesType>("all");
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
     let tasksFortodolist = tasks;
     if (filter === "active") {
         tasksFortodolist = tasks.filter(t => t.isDone === false);
     }
     if (filter === "completed") {
         tasksFortodolist = tasks.filter(t => t.isDone === true);
-    }
-
-    function changeFilter(value: FilterValuesType) {
-setFilter(value);
     }
 
     return (
